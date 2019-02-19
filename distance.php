d4d288653eacb698f882a1c26fa56177d36d68e0
Rebbeca Goncalves <30907967+GTRebbeca@users.noreply.github.com>
2019-02-19 16:34:42 -0600

    Add files via upload

--- distance.php
+++ distance.php
@@ -2,7 +2,7 @@
 
 function getDistance($lat1, $long1, $lat2, $long2)
 {
-$earth = 6371; //km change accordingly
+$earth = 6371; //km 
 //$earth = 3960; //miles
 
 //Point 1 cords
@@ -13,7 +13,7 @@ $long1= deg2rad($long1);
 $lat2 = deg2rad($lat2);
 $long2= deg2rad($long2);
 
-//Haversine Formula
+//Formula
 $dlong=$long2-$long1;
 $dlat=$lat2-$lat1;
 
@@ -28,8 +28,6 @@ $d=round($earth*$c);
 
 return $d;
 }
-
-// pull cords out of database
 
 echo "Distance in km from CB2 to SS4: ".getDistance(19.3939950917454, -99.06999009682619, 19.3878893, -99.17448709999996);
 ?>
